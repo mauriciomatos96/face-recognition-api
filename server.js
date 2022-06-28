eae95d11becf1de62b800de0ab2d413b6b0665bd
Mauricio <mauricio8_1996@hotmail.com>
2022-06-28 13:28:49 -0400

    image endpoint

--- server.js
+++ server.js
@@ -66,6 +66,21 @@ app.post("/register", (req, res) => {
     res.json(database.users[database.users.length - 1])
 })
 
+app.post("/image", (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(404).json("not found");
+    }
+})
+
 app.listen(3000, () => {
     console.log("The app is running");
 })
